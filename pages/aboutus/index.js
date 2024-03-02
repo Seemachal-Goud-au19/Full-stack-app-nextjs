7156abd0cf37e164a95fddce724264de4f97a2c8
Seemachal-Goud-au19 <simanchal9566@gmail.com>
2024-03-02 20:44:12 +0530

    Final touch to About US page

---
+++ pages/aboutus/index.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import Link from 'next/link';
+
+const details = [
+  { id: 1, name: 'Yash', role: 'Senior Developer' },
+  { id: 2, name: 'Vaibhav', role: 'Backend Developer' },
+  { id: 3, name: 'Suresh', role: 'Frontend Developer' }
+];
+const aboutus = () => {
+  return (
+    <div>
+      <ul>
+        {details.map((developer) => (
+          <li key={developer.id}><Link href={`/aboutus/${developer.id}`}>{developer.name}</Link></li>
+        ))}
+      </ul>
+    </div>
+  )
+}
+
+export default aboutus
